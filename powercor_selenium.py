fe46dda04f96d30a25acc8fe34d7ba55f6b29481
stugr <nick@stugr.com>
2020-05-06 15:28:34 +1000

    Removed some headers that aren't required

--- powercor_selenium.py
+++ powercor_selenium.py
@@ -175,15 +175,7 @@ with webdriver.Chrome(executable_path=chromedriver, options=options) as driver:
     download_url = 'https://customermeterdata.portal.powercor.com.au/customermeterdata/CADRequestMeterData?selNMI={}'.format(form_data['j_id0:SiteTemplate:j_id158:selNMI'])
 
     headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-        "cache-control": "max-age=0",
         "content-type": "application/x-www-form-urlencoded",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
         "referrer": download_url,
     }
 
